c605ddbb2f5bedd6249cfb75ef0e4e02badb4420
william <wiiliamchung360@gmail.com>
2024-12-27 20:15:13 -0800

    Update build version + release bundle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.wchung.qrshare"
         minSdk = 31
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.0.6"
+        versionCode = 9
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
