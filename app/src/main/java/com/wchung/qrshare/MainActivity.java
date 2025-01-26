e7349c2383aa3b157446c974a47bfadd7dc5195b
william <wiiliamchung360@gmail.com>
2025-01-25 22:58:45 -0800

    Don't use ?attr values for icons. Use drawable{,-night} resource files instead
    For some reason the share sheet icon will not pick up ?attr values

--- app/src/main/java/com/wchung/qrshare/MainActivity.java
+++ app/src/main/java/com/wchung/qrshare/MainActivity.java
@@ -1,11 +1,13 @@
 package com.wchung.qrshare;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,6 +21,7 @@ import androidx.core.view.WindowInsetsCompat;
 import com.google.zxing.common.BitMatrix;
 
 import java.io.File;
+import java.util.Objects;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -53,31 +56,35 @@ public class MainActivity extends AppCompatActivity {
 
         tv = findViewById(R.id.qr_subtitle);
         iv = findViewById(R.id.imageViewQRCode);
-        iv.setOnClickListener(this::hide_keyboard);
+        iv.setOnClickListener(this::clear_focus);
     }
 
     @Override
     protected void onNewIntent(@NonNull Intent intent) {
         super.onNewIntent(intent);
-        Log.i("QR test: onNewIntent", intent.toString());
+        Log.i("onNewIntent", intent.toString());
+        Log.i("onNewIntent", Objects.requireNonNull(intent.getAction()));
         stringForQRcode = getStringFromIntent(getIntent());
-        Log.i("QR test: stringForQRcode", stringForQRcode);
 
         // create a new QR code from the string
         // and set the string for the text edit
     }
 
-    private void hide_keyboard(View view){
+    private void clear_focus(View view){
+        //String tvText = tv.getText().toString();
+        //Log.i("QR test: clear_focus", tvText);
         tv.clearFocus();
     }
 
     private String getStringFromIntent(Intent intent) {
         String intentAction = intent.getAction();
         String intentType = intent.getType();
-        if (intent != null && Intent.ACTION_SEND == intentAction && intentType != null) {
+        Log.i("getStringFromIntent", "intentAction: " + intentAction);
+        Log.i("getStringFromIntent", "intentType: " + intentType);
+        if (intentAction != null && intentType != null) {
             if ("text".equals(intentType.split("/")[0])) {
                 String intentText = intent.getStringExtra(Intent.EXTRA_TEXT);
-                Log.i("QR test: getStringFromIntent", "intentText: " + intentText);
+                //Log.i("QR test: getStringFromIntent", "intentText: " + intentText);
                 return intentText;
             } else {
                 Toast.makeText(getApplicationContext(), R.string.unsupported_mimetype
@@ -85,13 +92,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return "";
-    }
-
-    @Override
-    protected void onPause(){
-        super.onPause();
-        //Log.i("onPause", "leaving... bye~!");
-        //finish();
     }
 
 }
