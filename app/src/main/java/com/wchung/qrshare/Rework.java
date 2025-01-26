f91786b7502e5bf4f6e1101ba141df56f60a9c02
william <wiiliamchung360@gmail.com>
2025-01-25 17:12:49 -0800

    Try out using TextEdit instead of using the material design text edit
    I like the material design text edit and the animation it has for the
    hint text, but the text gets cropped if the length is too long.

--- app/src/main/java/com/wchung/qrshare/Rework.java
+++ app/src/main/java/com/wchung/qrshare/Rework.java
@@ -1,57 +1,22 @@
 package com.wchung.qrshare;
 
-import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
-
-import android.content.ClipData;
-import android.content.ClipboardManager;
-import android.content.Context;
 import android.content.Intent;
-import android.content.ContentResolver;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
-import android.view.ContextMenu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.material.textfield.TextInputLayout;
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.EncodeHintType;
-import com.google.zxing.WriterException;
-import com.google.zxing.common.BitArray;
 import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.QRCodeWriter;
-import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-
-import android.view.MotionEvent;
-import android.view.LayoutInflater;
-import android.widget.PopupWindow;
 
 
 public class Rework extends AppCompatActivity {
@@ -60,12 +25,13 @@ public class Rework extends AppCompatActivity {
     private TextView tv = null;
     private ImageView iv = null;
     private File cacheFile;
+    private String stringForQRcode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -75,12 +41,29 @@ public class Rework extends AppCompatActivity {
         // Do something about intent captures
         // Create a function that handles intent captures and returns the text values in a string
         // The function will also need to handle onNewIntent() as well
+        stringForQRcode = getStringFromIntent(getIntent());
 
         // Do something that converts the string to a QR code
         // Create a function that takes a string and creates a QR code to cacheFile
 
         // Then create a function that handles text changes and updates the QR code
 
+    }
+
+    private String getStringFromIntent(Intent intent) {
+        String intentAction = intent.getAction();
+        String intentType = intent.getType();
+        if (intent != null && Intent.ACTION_SEND == intentAction && intentType != null) {
+            if ("text".equals(intentType.split("/")[0])) {
+                String intentText = intent.getStringExtra(Intent.EXTRA_TEXT);
+                //Log.i("QR test: getStringFromIntent", "intentText: " + intentText);
+                return intentText;
+            } else {
+                Toast.makeText(getApplicationContext(), R.string.unsupported_mimetype
+                        + intentType, Toast.LENGTH_LONG).show();
+            }
+        }
+        return "";
     }
 
     @Override
